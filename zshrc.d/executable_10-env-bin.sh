6788f46cb1a9869489bd03bb66d6c6cff9774ae1
Ian Buchanan <ibuchanan@atlassian.com>
2023-12-19 16:25:01 -0600

    feat: add npm-global to better manage global npm packages

--- zshrc.d/executable_10-env-bin.sh
+++ zshrc.d/executable_10-env-bin.sh
@@ -8,3 +8,9 @@ if [[ -d "$HOME/.local/bin" ]]; then
     export PATH="$HOME/.local/bin:$PATH"
 fi
 
+# npm-global
+# My own approach to better installs than `npm install -g`
+NPM_GLOBAL="$HOME/npm-global"
+if [[ -d "$NPM_GLOBAL/node_modules/.bin" ]]; then
+    export PATH="$NPM_GLOBAL/node_modules/.bin:$PATH"
+fi
