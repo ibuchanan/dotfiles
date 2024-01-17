0e5ad677d2d397a61ce2b7c54a307bbc66a428d8
Ian Buchanan <ibuchanan@atlassian.com>
2024-01-17 12:49:29 -0600

    feat: add forge autocomplete

---
+++ zshrc.d/91-shell-forge.sh
@@ -0,0 +1,10 @@
+
+# [Atlassian Forge](https://go.atlassian.com/forge)
+# Install
+# cd npm-global && npm install
+# Or:
+# npm install -g @forge/cli
+if [[ -x "$(command -v forge)" ]]; then
+    #. <(forge --completion)
+    eval "$(forge --completion)"
+fi
