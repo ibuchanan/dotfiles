0e2aa8db593de0180a91554a509bd8278a133d51
Ian Buchanan <ibuchanan@atlassian.com>
2024-03-24 19:11:48 -0500

    fix: suppress brew hints

--- zshrc.d/executable_10-env-brew.sh
+++ zshrc.d/executable_10-env-brew.sh
@@ -9,5 +9,7 @@ else
 fi
 if [[ -x "$(command -v "$BREWCMD")" ]]; then
     eval "$("$BREWCMD" shellenv)"
+    export HOMEBREW_NO_ENV_HINTS=1
+    export HOMEBREW_NO_INSECURE_REDIRECT=1
     FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 fi
