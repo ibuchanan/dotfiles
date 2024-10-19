90396dd4ad778f222dec78484b20fb6f12c1323b
Ian Buchanan <ibuchanan@atlassian.com>
2024-10-19 07:04:31 -0500

    fix: dashes not allowed in env vars

--- zshrc.d/executable_10-env-vars.sh
+++ zshrc.d/executable_10-env-vars.sh
@@ -2,10 +2,11 @@
 # Personal environment variables
 export EDITOR="code --wait"
 export GITHUB_USER=ibuchanan
-export EMAIL=ibuchanan@atlassian.com
+export EMAIL="ibuchanan@atlassian.com"
 export NAME="Ian Buchanan"
 
 # NPM
-export NPM_CONFIG_init-author-email=$EMAIL
-export NPM_CONFIG_init-author-name=$NAME
-export NPM_CONFIG_init-license="Apache-2.0"
+export NPM_CONFIG_INIT_AUTHOR_EMAIL="$EMAIL"
+export NPM_CONFIG_INIT_AUTHOR_NAME="$NAME"
+export NPM_CONFIG_INIT_LICENSE="Apache-2.0"
+export NPM_CONFIG_INIT_VERSION="0.0.0"
