d14f78a57ac8a52a316789207d93cb05c357ce4c
Ian Buchanan <ibuchanan@atlassian.com>
2025-01-18 17:09:43 -0600

    fix: remove pkgx per v2

--- zshrc.d/executable_11-env-pkgx.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-# [pkgx](https://pkgx.sh/)
-# Install
-# brew install pkgxdev/made/pkgx
-if [[ -x "$(command -v pkgx)" ]]; then
-    # `source <(pkgx --shellcode)`
-    eval "$(pkgx --shellcode)"
-fi
