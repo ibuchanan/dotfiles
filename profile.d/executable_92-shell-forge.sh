7633f3aca086b0f9e07059785465d58e90233acc
Ian Buchanan <ibuchanan@atlassian.com>
2025-09-15 13:28:38 +0200

    feat: fbs shortcut for forge-boostrap command

---
+++ profile.d/executable_92-shell-forge.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+# [Atlassian Forge](https://go.atlassian.com/forge)
+# Install
+# cd npm-global && npm install
+# Or:
+# npm install -g @forge/cli
+if [[ -x "$(command -v forge)" ]]; then
+    #. <(forge --completion)
+    eval "$(forge --completion)"
+fi
+
+# [Using env vars to login](https://developer.atlassian.com/platform/forge/getting-started/#using-environment-variables-to-login)
+# Uncomment and fill in if `forge login` does not work.
+# export FORGE_EMAIL=""
+# export FORGE_API_TOKEN=""
+
+# [Forge Bootstrap](https://github.com/ibuchanan/forge-bootstrap)
+# Utilities for bootstrapping new Node and Atlassian Forge apps
+export FORGE_BOOTSTRAP_HOME="$HOME/dev/git/github.com/ibuchanan/forge-bootstrap"
+alias forge-bootstrap="mask --maskfile $FORGE_BOOTSTRAP_HOME/maskfile.md"
+alias fbs="mask --maskfile $FORGE_BOOTSTRAP_HOME/maskfile.md"
