c5321e403fa26e24964f02941a0f898516f2dec5
Ian Buchanan <ibuchanan@atlassian.com>
2025-10-16 06:46:15 -0500

    feat[python]!: switch from pyenv to uv for system python management

--- profile.d/executable_10-env-bin.sh
+++ profile.d/executable_10-env-bin.sh
@@ -8,13 +8,15 @@ if [[ -d "$HOME/.orbit/bin" ]]; then
 fi
 
 # npm-global
-# My own approach to better installs than `npm install -g`
+# Better (more explicit) installs than `npm install -g`
 NPM_GLOBAL="$HOME/npm-global"
 if [[ -d "$NPM_GLOBAL/node_modules/.bin" ]]; then
     export PATH="$NPM_GLOBAL/node_modules/.bin:$PATH"
 fi
 
-# Pip --user commands
+# Python commands from:
+# pip --user
+# uv python install --default
 if [[ -d "$HOME/.local/bin" ]]; then
     export PATH="$HOME/.local/bin:$PATH"
 fi
