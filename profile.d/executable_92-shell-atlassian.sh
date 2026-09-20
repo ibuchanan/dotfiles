a0d4fbefdafc6cf698b984608959d8b3ab0438fd
Ian Buchanan <ibuchanan@atlassian.com>
2026-09-20 09:51:26 -0500

    fix(atlkey): startup bug and logging

--- profile.d/executable_92-shell-atlassian.sh
+++ profile.d/executable_92-shell-atlassian.sh
@@ -18,14 +18,4 @@ if [[ $- == *i* ]]; then
 		export PATH="$HOME/.orbit/bin:$PATH"
 	fi
 
-    # Atlassian SSH agent for YubiKey notifications
-    # https://hello.atlassian.net/wiki/spaces/cskb/pages/5250745730/Installing+the+Atlassian+SSH+agent
-    # https://bitbucket.org/asecurityteam/atlkey/src/main/
-    ATLKEYCMD="$HOME/bin/atlkey"
-    if [[ -x "$ATLKEYCMD" ]] && [[ "$OSTYPE" == "darwin"* ]] && [ ! -S /tmp/atlkey.sock ]; then
-        (
-            ~/bin/atlkey &>/dev/null &
-            disown
-        )
-    fi
 fi
